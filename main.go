4e20ceaafe5ddcc490e2297f68724eb8e6b12184
SealTV <seal.matob@gmail.com>
2017-05-31 00:41:41 +0300

    Remove unused code. Add index page

--- main.go
+++ main.go
@@ -4,56 +4,37 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/SealTV/handmade-shope/daemon"
 	"github.com/SealTV/handmade-shope/db"
 	"github.com/SealTV/handmade-shope/model"
+	"github.com/SealTV/handmade-shope/ui"
 	"github.com/gin-gonic/gin"
 )
 
-var assetsPath string
 var router *gin.Engine
 var m *model.Model
 
-func processFlags() *daemon.Config {
-	cfg := &daemon.Config{}
+type config struct {
+	Host string
+	DB   db.MongoConfig
+}
 
-	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
-	flag.StringVar(&cfg.Db.Host, "host", "localhost:12017", "Mongo serve host")
-	flag.StringVar(&cfg.Db.Username, "user", "user", "Mongo user")
-	flag.StringVar(&cfg.Db.Password, "password", "", "Mongo password")
+func processFlags() *config {
+	cfg := &config{}
+
+	flag.StringVar(&cfg.Host, "listen", "localhost:3000", "HTTP listen spec")
+	flag.StringVar(&cfg.DB.Host, "host", "localhost:12017", "Mongo serve host")
+	flag.StringVar(&cfg.DB.Username, "user", "user", "Mongo user")
+	flag.StringVar(&cfg.DB.Password, "password", "", "Mongo password")
 	// flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=/var/run/postgresql dbname=gowebapp sslmode=disable", "DB Connect String")
-	flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
+	// flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
 	flag.Parse()
 	return cfg
 }
 
-func setupHttpAssets(cfg *daemon.Config) {
-	log.Printf("Assets served from %q.", assetsPath)
-	cfg.UI.Assets = http.Dir(assetsPath)
-}
-
 func initializeRoutes() { // определение роута главной страницы
-	router.GET("/", showIndexPage)
-}
-
-// Define the route for the index page and display the index.html template
-// To start with, we'll use an inline route handler. Later on, we'll create
-// standalone functions that will be used as route handlers.
-func showIndexPage(c *gin.Context) {
-	// Call the HTML method of the Context to render a template
-	c.HTML(
-		// Set the HTTP status to 200 (OK)
-		http.StatusOK,
-		// Use the index.html template
-		"index.html",
-		// Pass the data that the page uses (in this case, 'title')
-		gin.H{
-			"title": "Home Page",
-		},
-	)
+	router.GET("/", ui.InitIndexPage(m))
 }
 
 func main() {
@@ -61,12 +42,7 @@ func main() {
 	cfg := processFlags()
 	fmt.Println(cfg)
 
-	// setupHttpAssets(cfg)
-	// if err := daemon.Run(cfg); err != nil {
-	// 	log.Printf("Error in main(): %v", err)
-	// }
-
-	db, err := db.InitMongoDb(cfg.Db)
+	db, err := db.InitMongoDb(cfg.DB)
 	if err != nil {
 		log.Printf("Error initializing database: %v\n", err)
 		log.Fatal(err)
@@ -85,5 +61,5 @@ func main() {
 	initializeRoutes()
 
 	// Start serving the application
-	router.Run(cfg.ListenSpec)
+	router.Run(cfg.Host)
 }
--- ui/ui.go
+++ ui/ui.go
@@ -1,80 +1,37 @@
 package ui
 
 import (
-	"encoding/json"
-	"fmt"
-	"net"
 	"net/http"
-	"time"
 
 	"github.com/SealTV/handmade-shope/model"
+	"github.com/gin-gonic/gin"
 )
 
-type Config struct {
-	Assets http.FileSystem
-}
-
-func Start(cfg Config, m *model.Model, listener net.Listener) {
-
-	server := &http.Server{
-		ReadTimeout:    60 * time.Second,
-		WriteTimeout:   60 * time.Second,
-		MaxHeaderBytes: 1 << 16}
-
-	http.Handle("/", indexHandler(m))
-	http.Handle("/people", peopleHandler(m))
-	http.Handle("/js/", http.FileServer(cfg.Assets))
-
-	go server.Serve(listener)
-}
-
-const (
-	cdnReact           = "https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react.min.js"
-	cdnReactDom        = "https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react-dom.min.js"
-	cdnBabelStandalone = "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.24.0/babel.min.js"
-	cdnAxios           = "https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.min.js"
-)
-
-const indexHTML = `
-<!DOCTYPE HTML>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Simple Go Web App</title>
-  </head>
-  <body>
-    <div id='root'>
-		<h1>Hello world</h1>
-	</div>
-    <script src="` + cdnReact + `"></script>
-    <script src="` + cdnReactDom + `"></script>
-    <script src="` + cdnBabelStandalone + `"></script>
-    <script src="` + cdnAxios + `"></script>
-    <script src="/js/app.jsx" type="text/babel"></script>
-  </body>
-</html>
-`
-
-func indexHandler(m *model.Model) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, indexHTML)
-	})
-}
-
-func peopleHandler(m *model.Model) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		people, err := m.Users()
+// Define the route for the index page and display the index.html template
+// To start with, we'll use an inline route handler. Later on, we'll create
+// standalone functions that will be used as route handlers.
+func InitIndexPage(m *model.Model) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		users, err := m.GetAllUsers()
 		if err != nil {
-			http.Error(w, "This is an error", http.StatusBadRequest)
+			c.HTML(http.StatusInternalServerError, "idnex.html", gin.H{
+				"title": "Error",
+			})
+
 			return
 		}
 
-		js, err := json.Marshal(people)
-		if err != nil {
-			http.Error(w, "This is an error", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Fprintf(w, string(js))
-	})
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+				"users": users,
+			},
+		)
+	}
 }
